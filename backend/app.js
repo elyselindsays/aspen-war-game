3bbcdba90c713349f72ad8272e151f88d0e7814b
Elyse Steingold <elyselindsays@gmail.com>
2022-02-12 13:06:38 -0500

    process.env

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,34 @@ const morgan = require('morgan');
 const cors = require('cors');
 const router = express.Router();
 const routes = require('./routes');
-
-
 const { environment } = require('./config');
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  router.get('/', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../frontend', 'build', 'index.html')
-    );
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
   });
-  router.use(express.static(path.resolve("../frontend/build")));
-  router.get(/^(?!\/?api).*/, (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../frontend', 'build', 'index.html')
-    );
-  });
+
 }
+// if (process.env.NODE_ENV === 'production') {
+//   const path = require('path');
+//   router.get('/', (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, '../frontend', 'build', 'index.html')
+//     );
+//   });
+//   router.use(express.static(path.resolve("../frontend/build")));
+//   router.get(/^(?!\/?api).*/, (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, '../frontend', 'build', 'index.html')
+//     );
+//   });
+// }
 const isProduction = environment === 'production';
 
 const app = express();
